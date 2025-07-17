511f832c8edf06c88857f8804aac3b3081a9579a
MohitMorya2 <mohitmorya56@gmail.com>
2025-07-18 00:36:20 +0530

    Fix import case for UserContext in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import SocketProvider from "./context/SocketContext.jsx";
-import UserContext from "./context/userContext.jsx";
+import UserContext from "./context/UserContext.jsx";
 import CaptainContext from "./context/CaptainContext.jsx";
 import "./index.css";
 
